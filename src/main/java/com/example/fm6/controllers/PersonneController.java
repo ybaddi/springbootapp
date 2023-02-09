41126dfc71410fdb3af4c652340d571cabceaf37
youssef baddi <baddiyoussef85@gmail.com>
2023-02-09 11:40:10 +0100

    create getall function in the controller class #2

--- src/main/java/com/example/fm6/controllers/PersonneController.java
+++ src/main/java/com/example/fm6/controllers/PersonneController.java
@@ -1,9 +1,25 @@
 package com.example.fm6.controllers;
 
+import java.util.List;
+
+import com.example.fm6.dao.PersonneDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.fm6.models.Personne;
+
+@RestController
 public class PersonneController {
 	
-	// TODO    get /
 	
+	@Autowired
+	public PersonneDao personneDao;
+	
+	// TODO    get /
+	public List<Personne> getPersonne() {
+		return personneDao.findAll();
+	}
 	
 	// TODO Post
 	
